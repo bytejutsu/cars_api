99e22538c1412c33e497e19eaa74aa1568de5e0a
Dhia Nabli <dhianabli@outlook.com>
2021-12-03 19:12:34 +0100

    start the delegate research functionality

--- main.py
+++ main.py
@@ -9,19 +9,37 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-automobile_url = "https://www.automobile.tn/fr/occasion/recherche"
-autoplus_url = "https://www.auto-plus.tn/voitures-d-occasion.html"
-affare_url = "https://www.affare.tn/petites-annonces/tunisie/voiture-neuve-occassion-prix-tayara-a-vendre"
-baniola_url = "https://baniola.tn/"
-binbincar_url = "https://www.binbincar.tn/vente-voiture/tunisie"
-argusautomobile_url = "https://www.argusautomobile.tn/voiture-occasion-tunisie/"
+automobile_url = "https://www.automobile.tn/fr/occasion?keyword="
+affare_url = "https://www.affare.tn/petites-annonces/tunisie/voiture-neuve-occassion-prix-tayara-a-vendre?recherche="
+baniola_url = "https://baniola.tn/search?q="
 
 keywords = ["peugeot", "206"] # max 3
 
 
-with driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get("https://google.com/ncr")
-    driver.find_element(By.NAME, "q").send_keys("cheese" + Keys.RETURN)
-    first_result = wait.until(presence_of_element_located((By.CSS_SELECTOR, "h3")))
-    print(first_result.get_attribute("textContent"))
+def delegate_search(url_base, keywords):
+    return f"{url_base}{'+'.join(keywords)}"
+
+
+def automobile_search(keywords):
+    return delegate_search(automobile_url, keywords)
+
+
+def affare_search(keywords):
+    return delegate_search(affare_url, keywords)
+
+
+def baniola_search(keywords):
+    return delegate_search(baniola_url, keywords)
+
+
+def launch_browser():
+    driver.get(baniola_search(keywords))
+
+
+launch_browser()
+
+
+
+
+
+
